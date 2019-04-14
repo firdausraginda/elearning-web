9d7b520bb3efed0ddd19deb6dfa73088af1b2a48
firdausraginda <ragindafirdaus01@gmail.com>
2019-04-14 18:49:07 +0700

    home page done w/ out responsive

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,7 +4,7 @@
       <v-layout justify-center class="text-xs-center">
         <v-flex xs8>
           <h1 class="mt-4">Ayo Belajar Tentang ADDIE !</h1>
-          <h4 class="my-5 px-5">
+          <h4 class="my-5 px-5 second-color">
             <b> ADDIE merupakan </b> model pembelajaran yang disusun secara
             terprogram dengan urutan-urutan kegiatan yang sistematis dalam upaya
             pemecahan masalah belajar yang berkaitan dengan sumber belajar.
@@ -13,13 +13,13 @@
         </v-flex>
       </v-layout>
       <v-layout wrap>
-        <template v-for="(item, index) in items">
-          <v-flex xs4>
-            <v-card class="elevation-0 px-2 py-2 rounded-card card-lesson">
+        <template v-for="item in items">
+          <v-flex xs6 md4 :key="item.title">
+            <v-card class="elevation-0 px-2 py-2 rounded-card hover-card">
               <v-card-title justify-start>
                 <div justify-start>
                   <div class="title primer-color">{{ item.title }}</div>
-                  <div class="body-1 text--secondary mt-2">
+                  <div class="body-1 second-color mt-2">
                     {{ item.subtitle }}
                   </div>
                 </div>
@@ -28,14 +28,19 @@
           </v-flex>
         </template>
       </v-layout>
+      <v-layout justify-center class="text-xs-center mt-1">
+        <v-flex xs6 md2>
+          <v-card class="elevation-0 rounded-card hover-card pa-3" target="blank" href="https://github.com/firdausraginda/elearning-web">
+            <div class="body-1 primer-color">Dev Mode</div>
+            <div class="body-1 second-color">Lihat code web ini</div>
+          </v-card>
+        </v-flex>
+      </v-layout>
     </v-container>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
   name: "home",
   data() {
@@ -80,7 +85,7 @@ export default {
 </script>
 
 <style lang="scss">
-.card-lesson:hover {
+.hover-card:hover {
   cursor: pointer;
   box-shadow: -4px 4px 4px rgba(0, 0, 0, 0.25) !important;
 }
@@ -95,8 +100,10 @@ h1 {
   color: #018caa;
 }
 h4 {
-  color: #8f8f8f;
   font-weight: 400;
+}
+.second-color {
+  color: #8f8f8f;
 }
 .text--secondary {
   line-height: 1.4;
