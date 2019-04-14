c64c50113053e1b542681ef57233ac8b46f212bf
firdausraginda <ragindafirdaus01@gmail.com>
2019-04-14 08:40:30 +0700

    first commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,44 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    
+      <v-breadcrumbs :items="items" divider=">"></v-breadcrumbs>
+      <v-breadcrumbs :items="items" divider="-"></v-breadcrumbs>
+      <v-btn color="success">Success</v-btn>
+
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+  // @ is an alias to /src
+  import HelloWorld from '@/components/HelloWorld.vue'
 
-export default {
-  name: 'home',
-  components: {
-    HelloWorld
+  export default {
+    name: 'home',
+    components: {
+      HelloWorld
+    },
+    data() {
+      return {
+        items: [
+          {
+            text: 'Dashboard',
+            disabled: false,
+            href: 'breadcrumbs_dashboard'
+          },
+          {
+            text: 'Link 1',
+            disabled: false,
+            href: 'breadcrumbs_link_1'
+          },
+          {
+            text: 'Link 2',
+            disabled: true,
+            href: 'breadcrumbs_link_2'
+          }
+        ]
+      }
+    }
   }
-}
 </script>
